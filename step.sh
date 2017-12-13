06e540d7e6ec37e8a1c1b1da1ce51051d5929353
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-12-13 14:58:06 +0100

    Forward console output, download artifact

--- step.sh
+++ step.sh
@@ -8,12 +8,23 @@ echo "Logging in to Quamotion Cloud"
 QUAMOTION_URL=https://cloud.quamotion.mobi
 QUAMOTION_ACCESS_TOKEN=`curl -s -d "apiKey=$quamotion_api_key" ${QUAMOTION_URL}/api/login | jq -r '.access_token'`
 QUAMOTION_RELATIVE_URL=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" ${QUAMOTION_URL}/api/project | jq -r '.[0].relativeUrl'`
+QUAMOTION_PROJECT_NAME=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" ${QUAMOTION_URL}/api/project | jq -r '.[0].name'`
 echo "Connected to the Quamotion project at $QUAMOTION_URL$QUAMOTION_RELATIVE_URL"
 
 echo "Scheduling the test run"
 QUAMOTION_TEST_RUN_REQUEST="{ \"app\": { \"operatingSystem\": \"$app_os\", \"appId\": \"$app_id\", \"version\": \"$app_version\" } ,\"testPackage\": { \"name\": \"$test_package_name\", \"version\": \"$test_package_version\" }, \"deviceGroupId\": \"57b9dda4-d1e4-423f-89c7-6f523daecb2e\"}"
 QUAMOTION_TEST_RUN=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" -H "Content-Type: application/json" -d "$QUAMOTION_TEST_RUN_REQUEST" -X POST ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/testRun`
 echo "Successfully scheduled the test run"
+
+QUAMOTION_TEST_RUN_ID=`echo $QUAMOTION_TEST_RUN | jq -r '.testRunId'`
+QUAMOTION_TEST_JOBS=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/testRun/${QUAMOTION_TEST_RUN_ID}/jobs`
+QUAMOTION_TEST_JOB=`echo $QUAMOTION_TEST_JOBS | jq -r '.[0].id'`
+
+# Forward the job output to Bitrise
+wscat -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" -c "${QUAMOTION_URL}ws?project=${QUAMOTION_PROJECT_NAME}&jobId={QUAMOTION_TEST_JOB}"
+
+# Download the artifact zip file
+curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/job/${QUAMOTION_TEST_JOB}/artifacts -O quamotion-artifacts.zip
 
 #
 # --- Export Environment Variables for other Steps:
