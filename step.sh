00a0c0916ad830ba68810e4e2209527fedd06da7
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-12 11:56:02 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -8,11 +8,12 @@ QUAMOTION_RELATIVE_URL=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKE
 echo "Connected to the Quamotion project at $QUAMOTION_URL$QUAMOTION_RELATIVE_URL"
 
 echo "Scheduling the test run"
-QUAMOTION_TEST_RUN_REQUEST="{ \"app\": { \"operatingSystem\": \"$app_os\", \"appId\": \"$app_id\", \"version\": \"$app_version\" } ,\"testPackage\": { \"name\": \"$test_package_name\", \"version\": \"$test_package_version\" }, \"testScriptParameters\": \"$test_script_parameters\", \"deviceGroupId\": \"57b9dda4-d1e4-423f-89c7-6f523daecb2e\"}"
-QUAMOTION_TEST_RUN=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" -H "Content-Type: application/json" -d "$QUAMOTION_TEST_RUN_REQUEST" -X POST ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/testRun`
+echo { } | jq --arg app_os $app_os --arg app_id $app_id --arg app_version $app_version --arg test_package_name $test_package_name --arg test_package_version $test_package_version --arg test_script_parameters "$test_script_parameters" '. + { app: { operatingSystem: $app_os, appId: $app_id, version: $app_version }, testPackage: { name: $test_package_name, version: $test_package_version, testScriptParameters: $test_script_parameters }, deviceGroupId: "57b9dda4-d1e4-423f-89c7-6f523daecb2e" }' > test_run_request.json
+
+QUAMOTION_TEST_RUN=`curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" -H "Content-Type: application/json" -d @test_run_request.json -X POST ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/testRun`
 echo "Successfully scheduled the test run"
 echo ">> Request"
-echo "$QUAMOTION_TEST_RUN_REQUEST"
+cat test_run_request.json
 echo "<<Request"
 echo ">> Response"
 echo "$QUAMOTION_TEST_RUN"
@@ -35,21 +36,3 @@ wget -O - --header "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" ${QUAMOTION_U
 # Download the artifact zip file
 echo "Downloading build artifact to quamotion-artifacts.zip"
 curl -s -H "Authorization: Bearer $QUAMOTION_ACCESS_TOKEN" -o quamotion-artifacts.zip ${QUAMOTION_URL}${QUAMOTION_RELATIVE_URL}api/job/${QUAMOTION_TEST_JOB}/artifacts
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
